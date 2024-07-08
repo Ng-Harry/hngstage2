f58ac052e79ff743d7c58cabf52a3c29fdf6b213
Ng-Harry <ngereharry5@gmail.com>
2024-07-08 17:03:11 +0100

    update host

--- stage2project/settings.py
+++ stage2project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q!-*b2h$3)n73dhts()%$h3xj97tzkwg)bd&(%skk(lzhux2$t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh','*']
 
 
 # Application definition
